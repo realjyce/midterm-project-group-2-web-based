bf1427cbc04f217bbba89621f3ef439e227072a8
Jason Clarence <126641050+realjyce@users.noreply.github.com>
2023-10-22 00:08:47 +0900

    Update project.py

--- Project/project.py
+++ Project/project.py
@@ -3,10 +3,19 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sklearn.metrics as metrics
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from permetrics.regression import RegressionMetric  
 
 # TITLE
 st.title("Project 2B: Web-App Machine Learning with Python")
 
 # TEST
-readData = pd.read_csv('Data.csv')
-st.write(readData)
+readData = pd.read_csv('./project/Data.csv')
+diamond = sns.load_dataset('diamonds')
+st.write('Shape of Dataset', diamond.shape)
+menu = st.sidebar.radio("Menu",["Home", "Raw Data"])
+if menu=="Raw Data":
+        st.write(readData)
+
