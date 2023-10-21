525de29fcc3406668b5d068850dd3fba0c8a9fd1
Jason Clarence <126641050+realjyce@users.noreply.github.com>
2023-10-22 00:54:11 +0900

    10/22/2023

--- Project/project.py
+++ Project/project.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,14 +9,37 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from permetrics.regression import RegressionMetric  
 
-# TITLE
-st.title("Project 2B: Web-App Machine Learning with Python")
+# MENU-ING AND TITE
+menu = st.sidebar.radio("Menu",["Home", "Raw Data"])
+if menu=="Home":
+    title=st.title("Project 2B: Web-App Machine Learning with Python")
+    st.balloons()
+    st.write("'Hello World'")
+
+# DATA CACHE FUNCTION
+@st.cache_data
+def load_data(url):
+    df = pd.read_csv(url)
+    return df
+
+# MENU:HOME
 
 # TEST
-readData = pd.read_csv('./project/Data.csv')
 diamond = sns.load_dataset('diamonds')
-st.write('Shape of Dataset', diamond.shape)
-menu = st.sidebar.radio("Menu",["Home", "Raw Data"])
-if menu=="Raw Data":
-        st.write(readData)
 
+# MENU: RAW DATA
+if menu!="Raw Data":
+        st.write('Shape of Dataset', diamond.shape)
+else:
+    title = st.title('Raw Data for [Flood]')
+    df = load_data('./project/Data.csv')
+    st.dataframe(df)
+    st.button('Rerun')
+    
+hide_made_with_streamlit = """
+    <style>
+        #MainMenu{visibility: hidden;}
+        footer {visibility:hidden;}
+    </style>
+"""
+st.markdown(hide_made_with_streamlit, unsafe_allow_html=True)
