140c132aa0007e121976fade9d4d378023a38e70
Jason Clarence <126641050+realjyce@users.noreply.github.com>
2023-10-30 18:17:08 +0900

    imported css function for streamlit, cleaning code

--- Project/project.py
+++ Project/project.py
@@ -1,12 +1,21 @@
 import streamlit as st
 st.set_page_config(layout="wide",initial_sidebar_state = "expanded")
+from xgboost import XGBRegressor
 import pandas as pd
-from sklearn.model_selection import train_test_split
 import ssl
 import seaborn as sns
 import matplotlib.pyplot as plt
 from permetrics.regression import RegressionMetric
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+#Import CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+local_css("style.css")
 
 # Ignore SSL certificate verification
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -20,9 +29,7 @@ def load_data(url):
 df = load_data('./project/Data.csv')
 
 df_1 = df.iloc[:, :-1]
-df_2 = df.iloc[:-1, :]
 X = df_1
-y = df_2
 
 # MENU-ING AND TITLE
 menu = st.sidebar.radio("Menu",["Home", "Raw Data", "Model"])
@@ -50,6 +57,7 @@ if menu == "Raw Data":
 
     # Split the data into training and testing sets based on the selected ratio
     X_train, X_test, _, _ = train_test_split(X, X, test_size=1 - train_ratio, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, df.iloc[:, -1], test_size=1 - train_ratio, random_state=42)
 
     st.write("Data Shape:")
     
@@ -69,22 +77,7 @@ if menu == "Raw Data":
     st.button('Rerun')
     
 #MODEL
-if menu == "Model":
-    ratio_option = st.selectbox("Select Training and Testing Data Ratio", ["90:10", "80:20", "70:30", "60:40"])
-
-    # Map the selected ratio to a train-test split ratio
-    if ratio_option == "90:10":
-        train_ratio = 0.9
-    elif ratio_option == "80:20":
-        train_ratio = 0.8
-    elif ratio_option == "70:30":
-        train_ratio = 0.7
-    elif ratio_option == "60:40":
-        train_ratio = 0.6
-        
-    X_train, X_test, _, _ = train_test_split(X, X, test_size=1 - train_ratio, random_state=42) # Training, testing
-    y_train, y_test, _, _ = train_test_split(y, y, test_size=1 - train_ratio, random_state=42)
-    model = RandomForestRegressor(random_state = 1).fit(X_train, y_train) # Train model
+if menu=="Model":
     # Creating Scatter Plot for Model
     y_predict = model.predict(X_test)
     sns.scatterplot(x=X_test, y=y_predict)
